416e9ebf76cca18412740a23d5ce5c5af5474def
Fernanda Freitas Martinez <idea.fernanda@gmail.com>
2022-07-07 20:18:35 +0100

    add column picture_url to flats table

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@ p 'Creating flat'
 Flat.create!(
   name: 'Light & Spacious Garden Flat London',
   address: '10 Clifton Gardens London W9 1DT',
+  picture_url: 'https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   price_per_night: 75,
   number_of_guests: 3
