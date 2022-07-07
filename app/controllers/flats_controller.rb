dd7464d62fb30500e110d47dff46ab1f18521794
Fernanda Freitas Martinez <idea.fernanda@gmail.com>
2022-07-07 21:20:37 +0100

    styles and default image

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -14,6 +14,7 @@ class FlatsController < ApplicationController
 
   def index
     @flats = Flat.all
+    @default_image = 'https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
   end
 
   def show
